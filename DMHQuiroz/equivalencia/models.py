1d345189c293f7c49cd5d7358231fc09b1e617ff
Andres Quiroz <andresq1595@gmail.com>
2019-07-05 14:51:52 -0500

    Fix

--- DMHQuiroz/equivalencia/models.py
+++ DMHQuiroz/equivalencia/models.py
@@ -3,9 +3,9 @@ from products.models import Product
 
 class Equivalencia(models.Model):
     cbarras=models.CharField(max_length=200)
-    descripcion = models.CharField(max_length=200)
+    descripcion = models.CharField(max_length=255)
     cbarras_equivalente = models.CharField(max_length=200)
-    descripcion_equivalente = models.CharField(max_length=200)
+    descripcion_equivalente = models.CharField(max_length=255)
     cantidad_equivalente = models.FloatField(max_length=50)
 
 
