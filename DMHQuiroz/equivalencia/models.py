9892620675dc08971f3c33879e81f599d566bbdc
Andres Quiroz <andresq1595@gmail.com>
2019-07-05 14:54:51 -0500

    Fix

--- DMHQuiroz/equivalencia/models.py
+++ DMHQuiroz/equivalencia/models.py
@@ -2,11 +2,11 @@ from django.db import models
 from products.models import Product
 
 class Equivalencia(models.Model):
-    cbarras=models.CharField(max_length=200)
-    descripcion = models.CharField(max_length=255)
-    cbarras_equivalente = models.CharField(max_length=200)
-    descripcion_equivalente = models.CharField(max_length=255)
-    cantidad_equivalente = models.FloatField(max_length=50)
+    cbarras=models.TextField()
+    descripcion = models.TextField()
+    cbarras_equivalente = models.TextField()
+    descripcion_equivalente = models.TextField()
+    cantidad_equivalente = models.TextField()
 
 
     def __str__(self):
